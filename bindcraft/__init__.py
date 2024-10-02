e40a08b0365556d11e3f62993b57742589e96e5c
Goosang Yu <gsyu93@gmail.com>
2024-10-03 01:09:13 +0900

    ✨

---
+++ bindcraft/__init__.py
@@ -0,0 +1,6 @@
+
+__version__ = '0.1.0'
+
+# hatch build
+# twine upload --username __token__ dist/*
+# [Enter PyPI API token]
---
+++ bindcraft/utils/functional.py
@@ -0,0 +1,6 @@
+import os, sys
+
+def check_import():
+    '''Check all required packagies and return the results.'''
+    
+    return None
